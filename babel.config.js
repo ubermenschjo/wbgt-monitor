2dd1ae940e084bdd155e9f132270b2e0aac7b9f3
Jo JungLae <ubermenschjo@gmail.com>
2026-06-06 13:24:10 +0900

    fix: remove manual babel plugins causing declare field conflict
    babel-preset-expo already includes @babel/plugin-transform-typescript
    which must run BEFORE class-properties. Manual plugins broke the order.

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
-    ],
   };
 };
